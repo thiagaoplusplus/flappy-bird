3602bee21cce854ae6704d8a545820a77bea3d16
Thiago Foganholi <thiagaoplusplus@outlook.com>
2022-08-02 22:25:49 -0300

    first working implementation

--- js/flappy.js
+++ js/flappy.js
@@ -4,21 +4,15 @@
  * the css files/html head.
  * 
  * @param {*} element The HTML element to read the CSS property
- * @param {*} property The CSS property to read.
+ * @param {string} property The CSS property to read.
  * @returns The CSS property as a percentage and as a number type. If the property is specified in
  * pixels in the CSS files, then end result will be just the value in pixels converted to number
  * type.
  */
- function getCSSPropertyAsPercentage(element, property) {
-	// If we take the property value directly from getComputedStyle function, the value returned will
-	// be in pixels even if the property is specified in percentage in the CSS files. However,
-	// curiously, if the display property is 'none' then getComputedStyle will return the value as
-	// percentage as we want.
-	let originalDisplayValue = element.style.display
-	element.style.display = 'none'
-	let propertyValue = parseInt(window.getComputedStyle(element)[property])
-	element.style.display = originalDisplayValue
-	return propertyValue
+ function getCSSPropertyAsPercentage(element, parentElement, property) {
+	const elementPropertyValue = parseInt(window.getComputedStyle(element)[property])
+	const parentPropertyValue = parseInt(window.getComputedStyle(parentElement)[property])
+	return elementPropertyValue / parentPropertyValue * 100
 }
 
 function Rectangle(originX = 0, originY = 0, width = 0, height = 0) {
@@ -67,13 +61,13 @@ function Rectangle(originX = 0, originY = 0, width = 0, height = 0) {
 	this.isOverlapping = function(rectangle) {
 		const points = [
 			// Lower left corner
-			makePoint(originX,         originY),
+			makePoint(this._origin.x,               this._origin.y),
 			// Upper left corner
-			makePoint(originX,         originY + height),
+			makePoint(this._origin.x,               this._origin.y + this._height),
 			// Lower right corner
-			makePoint(originX + width, originY),
+			makePoint(this._origin.x + this._width, this._origin.y),
 			// Upper right corner
-			makePoint(originX + width, originY + height)
+			makePoint(this._origin.x + this._width, this._origin.y + this._height)
 		]
 
 		const startX = rectangle._origin.x
@@ -81,11 +75,12 @@ function Rectangle(originX = 0, originY = 0, width = 0, height = 0) {
 		const startY = rectangle._origin.y
 		const endY   = rectangle._origin.y + rectangle._height
 
-		points.forEach(point => {
+		return points.some(point => {
 			if ( ((point.x >= startX) && (point.x <= endX)) &&
-			     ((point.y >= startY) && (point.y <= endY)) )
-			{
+			     ((point.y >= startY) && (point.y <= endY)) ) {
 				return true
+			} else {
+				return false
 			}
 		})
 	}
@@ -120,99 +115,91 @@ function Obstacle(parent) {
 	parent.appendChild(obstacle)
 
 	// overview of the constructed structure:
-	// +------------+ <---+                      100% verticalPosition + height
+	// +------------+ <---+  <-------+           100% verticalPosition + height
+	// |            |     |          |
+	// |  topPipe   |     |      topPipeSize
+	// |            |     |          |
+	// +------------+     |  <-------+
 	// |            |     |
-	// |  topPipe   |     |
+	// |  opening   |  height
 	// |            |     |
 	// +------------+     |  <-------+
 	// |            |     |          |
-	// |  opening   |  height    openingSize
+	// | bottomPipe |     |      bottomPipeSize
 	// |            |     |          |
-	// +------------+     |  <-------+
-	// |            |     |          | 
-	// | bottomPipe |     |      openingPosition
-	// |            |     |          | 
 	// +------------+ <---+  <-------+             0% verticalPosition
 	// ^            ^
 	// |   width    |
 
-	const horizontalPosition = getCSSPropertyAsPercentage(this._obstacleNode, 'left')
-	const verticalPosition = 0
-	const width = getCSSPropertyAsPercentage(this._obstacleNode, 'width')
-	const height = 100
-	const openingSize = getCSSPropertyAsPercentage(this._openingNode, 'height')
-	const openingPosition = getCSSPropertyAsPercentage(this._bottonPipeNode, 'height')
-
-	this._bottomPipeRectangle = new Rectangle(
-		horizontalPosition, verticalPosition, width, openingPosition
-	)
-
-	const topPipeRectangleVerticalPosition = verticalPosition + openingPosition + openingSize
-	this._topPipeRectangle = new Rectangle(
-		horizontalPosition, topPipeRectangleVerticalPosition,
-		width, height - topPipeRectangleVerticalPosition
-	)
+	this._horizontalPosition = getCSSPropertyAsPercentage(this._obstacleNode, parent, 'left')
+	this._width = getCSSPropertyAsPercentage(this._obstacleNode, parent, 'width')
+	this._bottonPipeSize = getCSSPropertyAsPercentage(this._bottonPipeNode, parent, 'height')
+	this._topPipeSize = getCSSPropertyAsPercentage(this._topPipeNode, parent, 'height')
 
 	// JS properties to ease obstacle manipulation
 	Object.defineProperty(this, 'position', {
 		get() {
-			return this._bottomPipeRectangle.originX
+			return this._horizontalPosition
 		},
 		set(newValue) {
-			this._bottomPipeRectangle.originX = newValue
-			this._topPipeRectangle.originX = newValue
+			this._horizontalPosition = newValue
 			this._obstacleNode.style.left = `${newValue}%`
 		}
 	})
 
+	/**
+	 * Width property.
+	 * 
+	 * @remark Although the current game implementation uses fixed width, I'm implementing the
+	 *    'set' method for the case of a future reuse.
+	 */
 	Object.defineProperty(this, 'width', {
 		get() {
-			return this._bottomPipeRectangle.width
+			return this._width
 		},
 		set(newValue) {
-			this._bottomPipeRectangle.width = newValue
-			this._topPipeRectangle.width = newValue
+			this._width = newValue
 			this._obstacleNode.style.width = `${newValue}%`
 		}
 	})
 
-	Object.defineProperty(this, 'openingPosition', {
+	Object.defineProperty(this, 'bottomPipeSize', {
 		get() {
-			return this._bottomPipeRectangle.height
+			return this._bottonPipeSize
 		},
 		set(newValue) {
-			const openingSize = 
-				this._topPipeRectangle.originY - this._bottomPipeRectangle.height
-			const topPipeNewOriginY = newValue + openingSize
-			const obstacleHeight = 100
-
-			this._bottomPipeRectangle.height = newValue
-			this._topPipeRectangle.originY = topPipeNewOriginY
-			this._topPipeRectangle.height = obstacleHeight - topPipeNewOriginY
-
+			this._bottonPipeSize = newValue
 			this._bottonPipeNode.style.height = `${newValue}%`
-			// top pipe style is ajusted automatically by CSS rules
 		}
 	})
 
-	Object.defineProperty(this, 'openingSize', {
+	Object.defineProperty(this, 'topPipeSize', {
 		get() {
-			return this._topPipeRectangle.originY - this._bottomPipeRectangle.height
+			return this._topPipeSize
 		},
 		set(newValue) {
-			const topPipeNewOriginY = this._bottomPipeRectangle.height + newValue
-			const obstacleHeight = 100
-
-			this._topPipeRectangle.originY = topPipeNewOriginY
-			this._topPipeRectangle.height = obstacleHeight - topPipeNewOriginY
-
-			this._openingNode.style.height = `${newValue}%`
+			this._topPipeSize = newValue
+			this._topPipeNode.style.height = `${newValue}%`
 		}
 	})
 
 	Object.defineProperty(this, 'rectangles', {
 		get() {
-			return [this._bottomPipeRectangle, this._topPipeRectangle]
+			const obstacleVerticalPosition = 0
+			const obstacleHeight = 100
+			const bottomPipeVerticalPosition = obstacleVerticalPosition
+			const topPipeVerticalPosition = obstacleHeight - this._topPipeSize
+
+			const bottomPipeRectangle = new Rectangle(
+				this._horizontalPosition, bottomPipeVerticalPosition,
+				this._width, this._bottonPipeSize
+			)
+
+			const topPipeRectangle = new Rectangle(
+				this._horizontalPosition, topPipeVerticalPosition,
+				this._width, this._topPipeSize
+			)
+			return [bottomPipeRectangle, topPipeRectangle]
 		}
 	})
 }
@@ -244,9 +231,16 @@ function Bird(parent) {
 	parent.appendChild(bird)
 
 	this._birdNode = bird
-	this._horizontalPosition = getCSSPropertyAsPercentage(this._birdNode, 'left')
-	this._verticalPosition = getCSSPropertyAsPercentage(this._birdNode, 'bottom')
-	this._height = getCSSPropertyAsPercentage(this._birdNode, 'height')
+	this._horizontalPosition = getCSSPropertyAsPercentage(this._birdNode, parent, 'left')
+	this._verticalPosition = getCSSPropertyAsPercentage(this._birdNode, parent, 'bottom')
+	this._width = getCSSPropertyAsPercentage(this._birdNode, parent, 'width')
+	this._height = getCSSPropertyAsPercentage(this._birdNode, parent, 'height')
+
+	Object.defineProperty(this, 'width', {
+		get() {
+			return this._width
+		}
+	})
 
 	Object.defineProperty(this, 'height', {
 		get() {
@@ -273,36 +267,131 @@ function Bird(parent) {
 			this._birdNode.style.bottom = `${newValue}%`
 		}
 	})
+
+	Object.defineProperty(this, 'rectangles', {
+		get() {
+			return [
+				new Rectangle(this._horizontalPosition, this._verticalPosition,
+					this._width, this._height)
+			]
+		}
+	})
 }
 
 const gameStage = document.querySelector('.content [wm-flappy]')
 const score = new Score(gameStage)
 const bird = new Bird(gameStage)
 
-// Values in % of the stage size
+// Values here and below for positioning/sizing are in % of the stage size
+const STAGE_HORIZONTAL_UPPER_LIMIT = 100
+const STAGE_HORIZONTAL_LOWER_LIMIT = 0
+const STAGE_VERTICAL_UPPER_LIMIT = 100
+const STAGE_VERTICAL_LOWER_LIMIT = 0
+
 //const OBSTACLES_INITIAL_POSITION = 0 // start out of stage
 const OBSTACLES_WIDTH = 10
 const OBSTACLES_DISTANCE_BETWEEN = 15
 const OBSTACLES_POSITION_OFFSET = OBSTACLES_WIDTH + OBSTACLES_DISTANCE_BETWEEN
+const OBSTACLES_OPENING_SIZE = 30
+const OBSTACLES_OPENING_HEIGHT_UPPER_LIMIT = STAGE_VERTICAL_UPPER_LIMIT - OBSTACLES_OPENING_SIZE
+const OBSTACLES_OPENING_HEIGHT_LOWER_LIMIT = 0
 
 // Maximum number of obstacles visible on screen possible
-const OBSTACLES_MAX_NUMBER_OF = Math.floor(100 / OBSTACLES_POSITION_OFFSET) + 1
+const OBSTACLES_MAX_NUMBER_OF = Math.floor(
+	STAGE_HORIZONTAL_UPPER_LIMIT / OBSTACLES_POSITION_OFFSET) + 1
 
-
-const obstacle = Array(OBSTACLES_MAX_NUMBER_OF)
-
-for (i = 0; i < OBSTACLES_MAX_NUMBER_OF; i++) {
-	obstacle[i] = new Obstacle(gameStage)
-	// obstacle[i].position = OBSTACLES_INITIAL_POSITION
-}
-
-const BIRD_FIXED_HORIZONTAL_POSITION = 10 // in % of the stage size
+const BIRD_FIXED_HORIZONTAL_POSITION = 10
 bird.horizontalPosition = BIRD_FIXED_HORIZONTAL_POSITION
 
 const GAME_FRAME_INTERVAL = 30 // in ms
-const GAME_OBSTACLE_POSITION_DECREMENT = 0.3 // in % of the stage size
-const GAME_BIRD_VPOSITION_INCREMENT = 0.8 // in % of the stage size
-const GAME_BIRD_VPOSITION_DECREMENT = 0.5 // in % of the stage size
+const GAME_OBSTACLE_POSITION_DECREMENT = 0.3
+const GAME_BIRD_VPOSITION_INCREMENT = 0.8
+const GAME_BIRD_VPOSITION_DECREMENT = 0.5
+
+const GAME_FRAMES_BETWEEN_OBSTACLES =
+	Math.floor((OBSTACLES_DISTANCE_BETWEEN - bird.width) / GAME_OBSTACLE_POSITION_DECREMENT)
+const GAME_BIRD_MAX_UP_TRAVEL_BETWEEN_OBSTACLES =
+	GAME_FRAMES_BETWEEN_OBSTACLES * GAME_BIRD_VPOSITION_INCREMENT
+const GAME_BIRD_MAX_DOWN_TRAVEL_BETWEEN_OBSTACLES =
+	GAME_FRAMES_BETWEEN_OBSTACLES * GAME_BIRD_VPOSITION_DECREMENT
+const GAME_BIRD_TRAVEL_GAP = bird.height
+
+
+function setRandomOpeningPosition(obstacle, openingSize, minPosition, maxPosition) {
+	obstacle.bottomPipeSize = Math.random() * (maxPosition - minPosition) + minPosition
+	obstacle.topPipeSize = STAGE_HORIZONTAL_UPPER_LIMIT - (obstacle.bottomPipeSize + openingSize)
+}
+
+function setRandomOpeningPositionForNextObstacle(nextObstacle, previousObstacle) {
+
+	//                 Previous
+	//                          Next
+	//                         | |                                          | |
+	//        +-----> +-+\     | | <----------+        +---------> +-+\     | |
+	//        |       | | \    | |            |        |           | | \    | |
+	// bottomPipeSize | |  \   | |             Bird max            | |  \   +-+ <--+-------+
+	//                | |   \  | |            down travel          | |   \         |       |
+	//                | |    \ | |            |        |           | |    \       Gap      |
+	//                | |     \+-+ <----------+        +---------> | |     \    <--+     Opening
+	//                | |           (Gap = 0) |                    | |                    size
+	//                | |                     |                    | |                     |
+	//                | |                   Opening                | |      +-+ <----------+
+	//                | |                    size                  | |      | |
+	//                | |                     |                    | |      | |
+	//                | |      +-+ <----------+                    | |      | |
+	//                | |      | |                                 | |      | |
+	//                 Without Gap                                   With Gap
+	//                        (the greater the gap, the easier the game)
+
+	// From the image above we derive the calculus below:
+	let minBottomPipeSize = (previousObstacle.bottomPipeSize -
+		GAME_BIRD_MAX_DOWN_TRAVEL_BETWEEN_OBSTACLES - OBSTACLES_OPENING_SIZE) + GAME_BIRD_TRAVEL_GAP
+	if (minBottomPipeSize < OBSTACLES_OPENING_HEIGHT_LOWER_LIMIT) {
+		minBottomPipeSize = OBSTACLES_OPENING_HEIGHT_LOWER_LIMIT
+	}
+
+	//                  Previous
+	//                           Next
+	//                 | |      | |                                 | |      | |
+	//                 | |      +-+ <----------+                    | |      | |
+	//                 | |                     |                    | |      | |
+	//                 | |                  Opening                 | |      | |
+	//                 | |                   size                   | |      +-+ <----------+
+	//                 | |                     |                    | |                     |
+	//                 | |           (Gap = 0) |                    | |                     |
+	//                 | |     /+-+ <----------+        +---------> | |     /    <--+     Opening
+	//                 | |    / | |            |        |           | |    /       Gap     size
+	//                 | |   /  | |             Bird max            | |   /         |       |
+	//                 | |  /   | |             up travel           | |  /   +-+ <--+-------+
+	//                 | | /    | |            |        |           | | /    | |
+	//       +-------> +-+/     | | <----------+        +---------> +-+/     | |
+	//       |                  | |                                          | |
+	// (bottomPipeSize          | |                                          | |
+	//       +                  | |                                          | |
+	//  openingSize)            | |                                          | |
+	//                  Without Gap                                   With Gap
+	//                         (the greater the gap, the easier the game)
+
+	// From the image above we derive the calculus below:
+	let maxBottomPipeSize = (previousObstacle.bottomPipeSize + OBSTACLES_OPENING_SIZE +
+		GAME_BIRD_MAX_UP_TRAVEL_BETWEEN_OBSTACLES) - GAME_BIRD_TRAVEL_GAP
+	if (maxBottomPipeSize > OBSTACLES_OPENING_HEIGHT_UPPER_LIMIT) {
+		maxBottomPipeSize = OBSTACLES_OPENING_HEIGHT_UPPER_LIMIT
+	}
+
+	setRandomOpeningPosition(nextObstacle, OBSTACLES_OPENING_SIZE, minBottomPipeSize, maxBottomPipeSize)
+}
+
+const obstacles = Array(OBSTACLES_MAX_NUMBER_OF)
+
+obstacles[0] = new Obstacle(gameStage)
+setRandomOpeningPosition(obstacles[0], OBSTACLES_OPENING_SIZE,
+	OBSTACLES_OPENING_HEIGHT_LOWER_LIMIT, OBSTACLES_OPENING_HEIGHT_UPPER_LIMIT)
+for (i = 1; i < OBSTACLES_MAX_NUMBER_OF; i++) {
+	obstacles[i] = new Obstacle(gameStage)
+	setRandomOpeningPositionForNextObstacle(obstacles[i], obstacles[i - 1])
+}
+
 
 let userInputMouseClicked = false
 let userInputKeyPressed = false
@@ -327,29 +416,45 @@ document.onkeyup = () => {
 bird.verticalPosition = 50
 
 const obstacleStartOfCourse = 100 // 100%
-const obstacleEndOfCourse = 0 - obstacle[0].width // 0% - width
+const obstacleEndOfCourse = 0 - obstacles[0].width // 0% - width
 
-obstacle.forEach(o => o.position = obstacleStartOfCourse)
+let obstacleScoredAlready = false
+function didPlayerScoreOnePoint(bird, obstacle, obstacleScoredAlready) {
+	if (bird.horizontalPosition > (obstacle.position + obstacle.width)) {
+		return (!obstacleScoredAlready)
+	} else {
+		return false
+	}
+}
 
-window.setInterval(() => {
+obstacles.forEach(o => o.position = obstacleStartOfCourse)
 
-	obstacle[0].position -= GAME_OBSTACLE_POSITION_DECREMENT
-	for (let i = 1; i < obstacle.length; i++) {
-		if ((obstacle[i].position - obstacle[i - 1].position) >= OBSTACLES_POSITION_OFFSET) {
-			obstacle[i].position -= GAME_OBSTACLE_POSITION_DECREMENT
+let gameOver = false
+const intervalId = window.setInterval(() => {
+
+	// Obstacles position update
+	obstacles[0].position -= GAME_OBSTACLE_POSITION_DECREMENT
+	for (let i = 1; i < obstacles.length; i++) {
+		if ((obstacles[i].position - obstacles[i - 1].position) >= OBSTACLES_POSITION_OFFSET) {
+			obstacles[i].position -= GAME_OBSTACLE_POSITION_DECREMENT
 		}
 	}
 
-	if (obstacle[0].position <= obstacleEndOfCourse) {
-		obstacle[0].position = obstacleStartOfCourse
-		const swapTemp = obstacle[0]
+	// End of obstacle course condition
+	if (obstacles[0].position <= obstacleEndOfCourse) {
+		obstacles[0].position = obstacleStartOfCourse
+		const swapTemp = obstacles[0]
 		let i
-		for (i = 1; i < obstacle.length; i++) {
-			obstacle[i - 1] = obstacle[i]
+		for (i = 1; i < obstacles.length; i++) {
+			obstacles[i - 1] = obstacles[i]
 		}
-		obstacle[i - 1] = swapTemp
+		obstacles[i - 1] = swapTemp
+
+		setRandomOpeningPositionForNextObstacle(obstacles[i - 1], obstacles[i - 2])
+		obstacleScoredAlready = false
 	}
 
+	// Bird posision update
 	const birdUpperEndOfCourse = 100 - bird.height // i.e. 100% - height
 	const birdLowerEndOfCourse = 0
 
@@ -368,8 +473,19 @@ window.setInterval(() => {
 		}
 	}
 
+	// Score update
+	let playerScoredOnePoint = didPlayerScoreOnePoint(bird, obstacles[0], obstacleScoredAlready)
+	if (playerScoredOnePoint) {
+		score.increment()
+	}
+	obstacleScoredAlready |= playerScoredOnePoint
 
+	// Game over condition
+	const birdRectangle = bird.rectangles[0]
+	const obstacleRectangles = obstacles[0].rectangles
 
+	gameOver = obstacleRectangles.some(rectangle => birdRectangle.isOverlapping(rectangle))
+	if (gameOver) {
+		clearInterval(intervalId)
+	}
 }, GAME_FRAME_INTERVAL)
-
-//for ()
